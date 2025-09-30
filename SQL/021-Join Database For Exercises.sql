005d64c5412698637d806ef32b035400b39225f8
PM <didattica@gmx.com>
2025-09-30 12:18:41 +0200

    Join EOJ

--- SQL/021-Join Database For Exercises.sql
+++ SQL/021-Join Database For Exercises.sql
@@ -33,4 +33,37 @@ insert into Person values('Mike', 85, 35000);
 insert into Person values('Lisa', 75, 87000);
 
 
+-- maternità su persone
+-- elenca la madre, figlio, età del figlio
+SELECT mother, child, p.age AS "Età Figlio"
+FROM motherchild mc
+INNER JOIN person p
+ON mc.child = p.name
+;
+
+-- maternità su persone
+-- elenca la madre, figlio, età del figlio
+SELECT mother, p2.age AS "Età Madre", child, p1.age AS "Età Figlio"
+FROM motherchild mc
+INNER JOIN person p1
+ON mc.child = p1.name
+INNER JOIN person p2
+ON mc.mother = p2.name
+;
+
+SELECT mother, p2.age AS "Età Madre", child, p1.age AS "Età Figlio"
+FROM motherchild mc
+INNER JOIN person p2
+ON mc.mother = p2.name
+INNER JOIN person p1
+ON mc.child = p1.name
+;
+
+select p.name, mc.mother, fc.father
+from person p
+left join MotherChild mc
+on mc.child = p.name 
+left join FatherChild fc
+on fc.child = p.name and fc.child = mc.child
+order by p.name
 
